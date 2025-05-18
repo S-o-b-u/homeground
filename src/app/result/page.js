4f68fb9f947a86c6ba0accd70a5751a67f2c3229
Souvik Rahut <souvikrahutofficial@gmail.com>
2025-05-19 01:32:58 +0530

    f

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -1,5 +1,6 @@
+import ResultContent from "@/components/ResultContent";
 import { Suspense } from "react";
-import ResultContent from "../components/ResultContent";
+
 
 export default function ResultPage() {
   return (
